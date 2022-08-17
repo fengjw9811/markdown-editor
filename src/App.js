d425b51af88c4c592b267fbb76a662acaa0f2635
fengjw <2849425293@qq.com>
2022-08-17 18:44:34 +0800

    初始化配置

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import FileSearch from './components/FileSearch'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
+function App () {
+  const onFileSearch = () => {}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container-fluid">
+      <div className='row'>
+        <div className='col-3 bg-light left-panel'>
+          <FileSearch title="我的云文档" onFileSearch={onFileSearch}/>
+        </div>
+        <div className='col-9 bg-primary right-panel'>
+          <h1>this is the right</h1>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
